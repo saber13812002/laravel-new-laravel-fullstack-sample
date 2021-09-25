dc6a300a1cafde90fbca2b39b883ce97d6f4414f
saber13812002 <saber.tabatabaee@gmail.com>
2021-09-25 12:54:20 +0330

    route در فایل routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/{any}', 'welcome')->where('any','.*');
+
+//Route::get('/', function () {
+//    return view('welcome');
+//});
