e20f3efd9f2d43f8b289ce1a1b6af63de1dae872
saber13812002 <saber.tabatabaee@gmail.com>
2021-09-25 12:52:07 +0330

    محتویات فایل frontend/app.js

--- frontend/app.js
+++ frontend/app.js
@@ -1,32 +1,20 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
+import Vue from 'vue';
+import App from './views/App'
+import Component from './components/Components';
 
-require('./bootstrap');
+window._ = require('lodash');
 
-window.Vue = require('vue').default;
+window.axios = require('axios');
 
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+Object.keys(Components).forEach(item => {
+        Vue.component(item, Components[item])
+    }
+);
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.config.productionTip = false;
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-const app = new Vue({
-    el: '#app',
-});
+new Vue({
+    render: h => h(App),
+}).$mount('#app');
